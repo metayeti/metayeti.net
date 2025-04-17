861cb046f805ef295b3a9f36d5263e94f481d4e1
pulzed <danijel.durakovic@gmail.com>
2025-04-17 20:18:10 +0200

    Update footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,9 +1,9 @@
 <template>
 	<footer>
 		<div class="wrap h-full py-5 flex flex-col justify-center items-center min-[1020px]:items-end gap-1">
-			<span class="text-neutral-400">Code and design by Danijel Durakovic</span>
+			<span class="text-neutral-500">Coded and designed by Danijel Durakovic</span>
 			<span class="text-neutral-600">&copy; 2025 metayeti.net</span>
-			<span class="text-neutral-600">Built with <a href="https://github.com/metayeti/metayeti.net" target="_blank"><img class="vue-icon" src="@/assets/images/vue.png" alt=""> Vue.js</a></span>
+			<span class="text-neutral-600">Built with <a href="https://vuejs.org" target="_blank"><img class="vue-icon" src="@/assets/images/vue.png" alt=""> Vue.js</a></span>
 		</div>
 	</footer>
 </template>
