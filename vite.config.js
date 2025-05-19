13ebf013beaa9fc22eb1f95dd11a245de579c8be
pulzed <danijel.durakovic@gmail.com>
2025-05-19 22:41:12 +0200

    Fix permalink issue

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Use relative paths for easier deployment
-  base: './',
+  base: '/',
   plugins: [
     vue(),
     //vueDevTools(),
