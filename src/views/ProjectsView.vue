dda939450ac7f83a2134bfd3dcb5a2a665afc3f0
pulzed <danijel.durakovic@gmail.com>
2025-06-28 00:00:57 +0200

    Tab styletest 2

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -16,22 +16,20 @@ onMounted(async () => {
 
 <template>
 	<div class="flex flex-col gap-1">
-		<ul class="projects-tab-list">
-			<div class="tabs flex flex-row flex-wrap-reverse gap-1 justify-end">
-				<li v-for="(item, i) in projectsListing.categories" >
-					<button
-						:class="i === 0 ? 'active' : ''"
-						class="flex flex-row gap-3 items-center"
-					>
-						<span class="icon">
-							<font-awesome-icon :icon="'fa-solid fa-' + item.icon" class="size-5" />
-						</span>
-						<span class="display">
-							{{ item.display }}
-						</span>
-					</button>
-				</li>
-			</div>
+		<ul class="projects-tab-list flex flex-row flex-wrap-reverse gap-1">
+			<li v-for="(item, i) in projectsListing.categories">
+				<button
+					:class="i === 0 ? 'active' : ''"
+					class="flex flex-row gap-3 items-center"
+				>
+					<!-- <span class="icon">
+						<font-awesome-icon :icon="'fa-solid fa-' + item.icon" class="size-5" />
+					</span> -->
+					<span class="display">
+						{{ item.display }}
+					</span>
+				</button>
+			</li>
 		</ul>
 		<section class="projects-tab-view">
 			<p class="caption"><br>Clonk! Crank! Whizzzz!<br>Uh oh - the gears seem to be stuck.<br>The hamsters are already working on it.<br></p>
@@ -41,40 +39,44 @@ onMounted(async () => {
 
 <style scoped lang="scss">
 .projects-tab-list {
-	.tabs {
-		transform: translateX(8px);
-		button {
-			position: relative;
-			padding: 4px 20px;
-			//text-align: center;
-			cursor: pointer;
-			color: var(--my-content-accent-text);
-			border-top: 2px solid transparent;
-			font-family: "Titillium Web", sans-serif;
-			font-size: 0.9em;
+	transform: translateX(7px);
+	button {
+		position: relative;
+		//padding: 4px 20px;
+		padding: 5px 20px;
+		//text-align: center;
+		cursor: pointer;
+		color: var(--my-content-accent-text);
+		//border-top: 2px solid transparent;
+		font-family: "Titillium Web", sans-serif;
+		font-size: 0.9em;
 
-			transform: skewX(-25deg);
-			scale: 0.96;
+		border: 2px solid transparent;
+		transform: skewX(-20deg);
+		//scale: 0.92;
 
-			span {
-				transform: skewX(25deg);
-				&.icon, &.display {
-					color: var(--my-content-text-dimmed);
-				}
+		span {
+			transform: skewX(20deg);
+			&.icon {
+				color: var(--my-tab-icon);
 			}
-			&.active, &:hover {
-				scale: 1;
-				background-color: var(--my-content-accent);
-
-				.icon, .display {
-					color: var(--my-content-link-hover);
-				}
+			&.display {
+				color: var(--my-tab-label);
 			}
-			&.active {
-				border-top: 2px solid var(--my-content-link);
-				.icon {
-					color: var(--my-content-accent-text);
-				}
+		}
+		&.active, &:hover {
+			//scale: 1;
+			//background-color: var(--my-content-accent);
+			border-color: var(--my-content-accent);
+
+			.icon, .display {
+				color: var(--my-content-link-hover);
+			}
+		}
+		&.active {
+			border-bottom: 2px solid var(--my-content-flavor);
+			.icon {
+				color: var(--my-content-accent-text);
 			}
 		}
 	}
