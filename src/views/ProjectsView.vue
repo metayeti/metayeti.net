98452163394480c8914ab52b532970ac75f60658
pulzed <danijel.durakovic@gmail.com>
2025-06-28 13:44:21 +0200

    V3 tabs styles

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -15,16 +15,16 @@ onMounted(async () => {
 </script>
 
 <template>
-	<div class="flex flex-col gap-1">
-		<ul class="projects-tab-list flex flex-row flex-wrap-reverse gap-1">
+	<div class="flex flex-col">
+		<ul class="projects-tab-list flex flex-row flex-wrap-reverse gap-1 justify-end">
 			<li v-for="(item, i) in projectsListing.categories">
 				<button
 					:class="i === 0 ? 'active' : ''"
 					class="flex flex-row gap-3 items-center"
 				>
-					<!-- <span class="icon">
+					<span class="icon">
 						<font-awesome-icon :icon="'fa-solid fa-' + item.icon" class="size-5" />
-					</span> -->
+					</span>
 					<span class="display">
 						{{ item.display }}
 					</span>
@@ -39,24 +39,27 @@ onMounted(async () => {
 
 <style scoped lang="scss">
 .projects-tab-list {
-	transform: translateX(7px);
+	//transform: translateX(-10px);
 	button {
 		position: relative;
 		//padding: 4px 20px;
-		padding: 5px 20px;
+		padding: 15px 22px;
 		//text-align: center;
 		cursor: pointer;
 		color: var(--my-content-accent-text);
-		//border-top: 2px solid transparent;
+		//border: 2px solid transparent;
+		//border-bottom: 0;
 		font-family: "Titillium Web", sans-serif;
-		font-size: 0.9em;
+		font-size: 0.95rem;
+		font-weight: 600;
+			//background-color: transparent;
 
-		border: 2px solid transparent;
-		transform: skewX(-20deg);
-		//scale: 0.92;
+		//border: 2px solid transparent;
+		//transform: skewX(20deg);
+		scale: 0.96;
 
 		span {
-			transform: skewX(20deg);
+			//transform: skewX(-20deg);
 			&.icon {
 				color: var(--my-tab-icon);
 			}
@@ -65,24 +68,54 @@ onMounted(async () => {
 			}
 		}
 		&.active, &:hover {
-			//scale: 1;
+			scale: 1;
 			//background-color: var(--my-content-accent);
-			border-color: var(--my-content-accent);
+			//background-color: var(--my-content-accent);
+			//border-color: var(--my-content-accent);
 
 			.icon, .display {
 				color: var(--my-content-link-hover);
 			}
 		}
 		&.active {
-			border-bottom: 2px solid var(--my-content-flavor);
+			//border-top: 2px solid var(--my-content-link);
 			.icon {
-				color: var(--my-content-accent-text);
+				color: var(--my-content-link);
+			}
+			&::before {
+				content: '';
+				position: absolute;
+				width: 0;
+				height: 0;
+				right: 0;
+				bottom: 0;
+				//background-color: orange;
+				/*
+				border-top: transparent;
+				border-right: transparent;
+				border-left: 10px solid orange;
+				border-bottom: 10px solid orange;
+				*/
+				border-style: solid;
+				border-width: 0 0 10px 12px;
+				border-color: transparent transparent var(--my-content-link) transparent;
+			}
+			&::after {
+				content: '';
+				position: absolute;
+				width: 100%;
+				height: 2px;
+				bottom: -2px;
+				left: 0;
+				background-color: var(--my-content-link);
+				//clip-path: polygon(0 0, 100% 0, 93% 100%, 7% 100%);
 			}
 		}
 	}
 }
 .projects-tab-view {
 	border: 2px solid var(--my-content-accent);
+	//background-color: var(--my-content-accent);
 	padding: 10px;
 }
 </style>
