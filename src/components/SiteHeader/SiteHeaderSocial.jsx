005e931c60a1b751d7138eac456af70d1eea10ee
metayeti <metayetidev@gmail.com>
2026-03-01 00:27:26 +0100

    Implement tooltips in social bar

--- src/components/SiteHeader/SiteHeaderSocial.jsx
+++ src/components/SiteHeader/SiteHeaderSocial.jsx
@@ -13,7 +13,7 @@
 //
 //  Author:       Danijel Durakovic <metayetidev@gmail.com>
 //  Created:      2026-02-27
-//  Updated:      2026-02-28
+//  Updated:      2026-03-01
 //
 //  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //
@@ -37,6 +37,7 @@ export default function SiteHeaderSocial() {
 			<div className="site-header-social__links wrapped">
 				<a href="https://github.com/metayeti" target="_blank" className="site-header-social__link">
 					<IconGitHub />
+					<div className="site-header-social__link-tooltip">GitHub</div>
 				</a>
 				<a
 					href="https://www.facebook.com/profile.php?id=100095050669741"
@@ -44,24 +45,33 @@ export default function SiteHeaderSocial() {
 					className="site-header-social__link"
 				>
 					<IconFacebook />
+					<div className="site-header-social__link-tooltip">Facebook</div>
 				</a>
 				<a href="#" target="_blank" className="site-header-social__link">
 					<IconLinkedIn />
+					<div className="site-header-social__link-tooltip">LinkedIn</div>
 				</a>
 				<a href="#" target="_blank" className="site-header-social__link">
 					<IconX />
+					<div className="site-header-social__link-tooltip">X</div>
 				</a>
 				<a href="#" target="_blank" className="site-header-social__link">
 					<IconSteam />
+					<div className="site-header-social__link-tooltip">Steam</div>
 				</a>
 				<a href="#" target="_blank" className="site-header-social__link">
 					<IconItchIo />
+					<div className="site-header-social__link-tooltip">itch.io</div>
 				</a>
 				<a href="#" target="_blank" className="site-header-social__link">
 					<IconYouTube />
+					<div className="site-header-social__link-tooltip">YouTube</div>
 				</a>
 				<a href="#" target="_blank" className="site-header-social__link">
 					<IconSoundCloud />
+					<div className="site-header-social__link-tooltip site-header-social__link-tooltip--last">
+						SoundCloud
+					</div>
 				</a>
 			</div>
 		</div>
